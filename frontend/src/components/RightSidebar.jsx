298ab84647d25b3ed84cc87fc3004291802dc753
Ashish <zopjoker99@gmail.com>
2025-04-10 14:23:09 +0530

    Update RightSidebar.jsx

--- frontend/src/components/RightSidebar.jsx
+++ frontend/src/components/RightSidebar.jsx
@@ -1,27 +1,33 @@
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
-import SuggestedUsers from './SuggestedUsers';
+import { Link } from 'react-router-dom'
+import SuggestedUsers from './SuggestedUsers'
 
 const RightSidebar = () => {
-  const { user } = useSelector(store => store.auth);
+  const { user } = useSelector(store => store.auth)
+
   return (
-    <div className='w-fit my-10 pr-32'>
-      <div className='flex items-center gap-2'>
+    <aside className='hidden lg:block w-full max-w-[300px] px-4 my-8'>
+      <div className='flex items-center gap-4'>
         <Link to={`/profile/${user?._id}`}>
-          <Avatar>
-            <AvatarImage src={user?.profilePicture} alt="post_image" />
+          <Avatar className="h-12 w-12">
+            <AvatarImage src={user?.profilePicture} alt={`${user?.username}'s profile`} />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
         </Link>
-        <div>
-          <h1 className='font-semibold text-sm'><Link to={`/profile/${user?._id}`}>{user?.username}</Link></h1>
-          <span className='text-gray-600 text-sm'>{user?.bio || 'Bio here...'}</span>
+        <div className='overflow-hidden'>
+          <h1 className='font-semibold text-sm truncate'>
+            <Link to={`/profile/${user?._id}`}>{user?.username}</Link>
+          </h1>
+          <p className='text-gray-600 text-sm truncate'>{user?.bio || 'Bio here...'}</p>
         </div>
       </div>
-      <SuggestedUsers/>
-    </div>
+
+      <div className='mt-6'>
+        <SuggestedUsers />
+      </div>
+    </aside>
   )
 }
 
