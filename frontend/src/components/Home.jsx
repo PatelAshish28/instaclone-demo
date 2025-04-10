d5bd1fd8770ee08484d10321c714828f4d8c318c
Ashish <zopjoker99@gmail.com>
2025-04-10 14:12:19 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import Feed from './Feed'
-import { Outlet } from 'react-router-dom'
-import RightSidebar from './RightSidebar'
-import useGetAllPost from '@/hooks/useGetAllPost'
-import useGetSuggestedUsers from '@/hooks/useGetSuggestedUsers'
+import React from 'react';
+import Feed from './Feed';
+import { Outlet } from 'react-router-dom';
+import RightSidebar from './RightSidebar';
+import useGetAllPost from '@/hooks/useGetAllPost';
+import useGetSuggestedUsers from '@/hooks/useGetSuggestedUsers';
 
 const Home = () => {
-    useGetAllPost();
-    useGetSuggestedUsers();
-    return (
-        <div className='flex'>
-            <div className='flex-grow'>
-                <Feed />
-                <Outlet />
-            </div>
-            <RightSidebar />
-        </div>
-    )
-}
+  useGetAllPost();
+  useGetSuggestedUsers();
 
-export default Home
+  return (
+    <div className="flex flex-col lg:flex-row">
+      <div className="flex-grow">
+        <Feed />
+        <Outlet />
+      </div>
+      <div className="hidden lg:block">
+        <RightSidebar />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
