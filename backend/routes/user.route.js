b7d25b504738359dc91fb01c21397c21640019f9
Ashish <zopjoker99@gmail.com>
2025-04-10 16:36:57 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -10,6 +10,7 @@ router.route('/login').post(login);
 router.route('/logout').get(logout);
 router.route('/:id/profile').get(isAuthenticated, getProfile);
 router.route('/profile/edit').post(isAuthenticated, upload.single('profilePhoto'), editProfile);
+router.route('/search').get(isAuthenticated, searchUsers);
 router.route('/suggested').get(isAuthenticated, getSuggestedUsers);
 router.route('/followorunfollow/:id').post(isAuthenticated, followOrUnfollow);
 
