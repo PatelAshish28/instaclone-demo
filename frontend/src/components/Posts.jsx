4dcdecd7612545ff2d01e725c5b6307918e3d79e
Ashish <zopjoker99@gmail.com>
2025-04-10 14:19:25 +0530

    Update Posts.jsx

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -3,12 +3,15 @@ import Post from './Post'
 import { useSelector } from 'react-redux'
 
 const Posts = () => {
-  const {posts} = useSelector(store=>store.post);
+  const { posts } = useSelector(store => store.post)
+
   return (
-    <div>
-        {
-            posts.map((post) => <Post key={post._id} post={post}/>)
-        }
+    <div className="w-full px-2 sm:px-4 md:px-6 lg:px-8 max-w-2xl mx-auto mt-6">
+      {posts && posts.length > 0 ? (
+        posts.map(post => <Post key={post._id} post={post} />)
+      ) : (
+        <p className="text-center text-gray-500 text-sm mt-10">No posts available.</p>
+      )}
     </div>
   )
 }
