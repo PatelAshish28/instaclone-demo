ea2d66bb89d46210647bb15a1f981efbb1846466
Ashish <zopjoker99@gmail.com>
2025-04-10 14:45:54 +0530

    Added Darkmode

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,13 +4,15 @@ import { Outlet } from 'react-router-dom';
 import RightSidebar from './RightSidebar';
 import useGetAllPost from '@/hooks/useGetAllPost';
 import useGetSuggestedUsers from '@/hooks/useGetSuggestedUsers';
+import DarkModeToggle from './DarkModeToggle'; // 👈 Import the dark mode toggle
 
 const Home = () => {
   useGetAllPost();
   useGetSuggestedUsers();
 
   return (
-    <div className="flex flex-col lg:flex-row">
+    <div className="flex flex-col lg:flex-row bg-white dark:bg-black text-black dark:text-white min-h-screen transition-all">
+      <DarkModeToggle /> {/* 👈 Add toggle button here */}
       <div className="flex-grow">
         <Feed />
         <Outlet />
